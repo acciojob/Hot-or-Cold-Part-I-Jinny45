381c3717cf8986a05034529d029c67a7cfc0b6c1
Jinashree Gane <113277171+Jinny45@users.noreply.github.com>
2023-03-03 16:56:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,15 @@
 //your code here
-var btn = document.getElementById("btn");
-var numPtag = document.getElementBYId("num");
-var res = document.getElementById("respond");
-var inputValue = document.getElementById("guess").value;
+const randomNumber = Math.floor(Math.random() * 100) + 1; // generates a random number between 1 and 100
+const output = document.getElementById("respond");
+const guessInput = document.getElementById("guess");
 
-function randomNumGenerator(){
-	randomNum = Math.floor(Math.random()*41)-20;
-	document.getElementById("num").innerText = randomNum;
-	guessNum();
-	print();
+function checkGuess() {
+  const userGuess = parseInt(guessInput.value);
+  if (userGuess === randomNumber) {
+    output.innerHTML = "Congratulations! You guessed the correct number!";
+  } else if (userGuess > randomNumber) {
+    output.innerHTML = "Hotter";
+  } else {
+    output.innerHTML = "Colder!";
+  }
 }
-function guessNum(){
-	input = Number(document.getElementById("guess").value);
-}
-function print(){
-	if(Math.abs(randomNum - input)<=5){
-		document.getElementById("respond").innerText="Hot";
-	}
-	else {
-		document.getElementById("respond").innerHTML = "Cold";
-	}
-	
-}
-
-btn.addEventListener("click", randomNumGenerator);
